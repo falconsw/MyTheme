3e367624a52f2dd86b7b5a30aa6e9b559c7fe8a8
GitHub Action <action@github.com>
2022-02-15 18:07:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/falconsw/mytheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/falconsw/mytheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.falconsw.mytheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.falconsw.mytheme.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/falconsw/mytheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/falconsw/mytheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.falconsw.mytheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.falconsw.mytheme.MyBundle
 
 class MyProjectService(project: Project) {
 
